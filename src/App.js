80556033521082892cdbc241e00b1d03e5509c40
YehorBodenchuk <120697528+YehorBodenchuk@users.noreply.github.com>
2023-06-14 12:50:25 +0300

    Initialize react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
 function App() {
+
+  React.useEffect(() => {
+    const fetchHello = async () => {
+      const response = await axios.get('http://localhost:8080/api/hello');
+      console.log(response)
+    }
+
+    fetchHello().then(r => console.log('r: ', r))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hello world!</p>
     </div>
   );
 }
